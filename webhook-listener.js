9dcb347b36140862559c2aa185ec694006dbd391
Shaveen Bageloo <shaveenbageloo@Shaveens-MacBook-Air.local>
2026-04-02 22:06:14 +0200

    Initial code

---
+++ webhook-listener.js
@@ -0,0 +1,24 @@
+// webhook-listener.js
+const express = require('express');
+const { exec } = require('child_process');
+const crypto = require('crypto');
+const app = express();
+
+const SECRET = process.env.WEBHOOK_SECRET;
+
+app.post('/', express.raw({ type: 'application/json' }), (req, res) => {
+  console.log('Webhook called....');
+  const sig = req.headers['x-hub-signature-256'];
+  const hmac = 'sha256=' + crypto.createHmac('sha256', SECRET).update(req.body).digest('hex');
+  
+  if (sig !== hmac) return res.status(401).send('Unauthorized');
+  
+  res.sendStatus(200);
+  exec('/home/pi/myapp/update.sh', (err, stdout) => {
+    if (err) console.error('Update failed:', err);
+    else console.log('Updated:', stdout);
+  });
+});
+
+app.listen(9000);
+console.log('Webhook listening on Port 9000');
