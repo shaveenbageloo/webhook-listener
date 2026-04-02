ed74a64db80250190028623b9252640a65891f4d
Shaveen Bageloo <shaveenbageloo@Shaveens-MacBook-Air.local>
2026-04-02 22:36:18 +0200

    updated with password check

--- webhook-listener.js
+++ webhook-listener.js
@@ -8,16 +8,23 @@ const SECRET = process.env.WEBHOOK_SECRET;
 
 app.post('/', express.raw({ type: 'application/json' }), (req, res) => {
   console.log('Webhook called....');
+
   const sig = req.headers['x-hub-signature-256'];
   const hmac = 'sha256=' + crypto.createHmac('sha256', SECRET).update(req.body).digest('hex');
-  
-  if (sig !== hmac) return res.status(401).send('Unauthorized');
+
+  //console.log(SECRET,sig,hmac);
+
+  if (sig !== 'PASSWORD') return res.status(401).send('Unauthorized');
+  //if (sig !== hmac) return res.status(401).send('Unauthorized');
+  console.log('signature is okay'); 
   
   res.sendStatus(200);
   exec('/home/pi/myapp/update.sh', (err, stdout) => {
+  //exec('ls -lrt', (err, stdout) => {
     if (err) console.error('Update failed:', err);
     else console.log('Updated:', stdout);
   });
+
 });
 
 app.listen(9000);
